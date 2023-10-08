1fd9f95ed47098e99c5699ba6cbc6bdb96a3ee15
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-10-08 18:52:34 -0400

    got the font

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -55,8 +55,8 @@ export const TETROMINOS = {
 };
 
 export const randomTetromino = () => {
-    const tetrominos = `IJLOSTZ`;
+    const tetrominos = 'IJLOSTZ';
     const randTetromino =
-    tetrominos[Math.floor(Math.random() = tetrominos.length)];
+    tetrominos[Math.floor(Math.random() * tetrominos.length)];
     return TETROMINOS[randTetromino];
 }
